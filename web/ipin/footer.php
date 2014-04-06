b596776dcbc59d616af1e800c93c321c8fc65eb1
amandasmcvey <amandasmcvey@ufl.edu>
2014-04-06 17:30:09 -0400

    Bootstrap Website
    Feed and other pages

---
+++ web/ipin/footer.php
@@ -0,0 +1,8 @@
+<div id="footer" class="container">
+	<div class="text-align-center">&copy; <a href="<?php echo home_url() ?>" title="Home"><?php bloginfo('name') ?></a> - <?php bloginfo('description') ?> <?php if (is_home()) { ?>/ Design: <a href="http://ericulous.com/2012/09/21/wp-theme-ipin-pinterest-clone/" title="Smashing Wordpress Themes">Smashing Wordpress Themes</a><?php } ?></div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
