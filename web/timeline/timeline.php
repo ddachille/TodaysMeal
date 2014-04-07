e920bd67bbfda7c6bea445cca2edcf521a89bcec
JoeyS7 <joeysiracusa@gmail.com>
2014-04-07 10:21:54 -0400

    Still debugging query
    This is *hard*

--- web/timeline/timeline.php
+++ web/timeline/timeline.php
@@ -40,7 +40,11 @@
   						echo "An error occured.\n";
   						exit;
   					}
-  					echo $arr[0] . $arr[1] . $arr[2] . $arr[3] . $arr[4];
+  					echo $arr[0];
+  					echo $arr[1];
+  					echo $arr[2]; 
+  					echo $arr[3];
+  					echo $arr[4];
   					
   					$arr = pg_fetch_row($result);
   				#	$query2 = sprintf("SELECT username FROM Users WHERE uid = " . $arg[0]);
