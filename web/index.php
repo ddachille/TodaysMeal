19f2bec0078ee3164c3ea07ec12b04b3fa340249
JoeyS7 <joeysiracusa@gmail.com>
2014-04-07 10:13:39 -0400

    Adjusting query

--- web/index.php
+++ web/index.php
@@ -21,7 +21,11 @@
   	}
   	
   	$arr = pg_fetch_row($result);
-  	echo $arr[0];
+  	$query2 = sprintf("SELECT username FROM Users WHERE uid = " . $arg[0]);
+  	$result2 = pg_query($conn, $query2);
+  	$arr2 = pg_fetch_row($result2);
+  	echo "Username: " . $arr2[0];
+  	echo $arr[4];
   	
 
 ?>
