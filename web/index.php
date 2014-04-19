53678d645a0d21d5ca69fd5f2204463831f8f567
ddachille <ddachille@ufl.edu>
2014-04-19 14:50:27 -0400

    Index and Registration
    Works, except user is automatically set to private even when "is
    private" is specified as false. (need to fix)

--- web/index.php
+++ web/index.php
@@ -1,41 +1,17 @@
 #!/usr/local/bin/php
 
 <html>
-	<head><title>PHP Test</title></head>
+	<head><title>Register</title></head>
 <body>
 
  
-<form name="index" action="index.php" method="POST" >  
-Username:<input type="text" name="username" />  
-Password:<input type="text" name="hashedpw" />
-Set Profile to Private? (true or false): <input type="text" name= "isPrivate" />     
+<form name="index" action="registration.php" method="POST" >  
+Username:<input type="text" name="username" /></br>  
+Password:<input type="text" name="hashedpw" /></br>
+Set Profile to Private? (true or false): <input type="text" name= "isPrivate" /></br>     
 <input type="submit" />  
 </form>  
 
-
-<?php 
-
-	$db = pg_connect("host=postgres dbname=meal user= js7 password=MealAdminOfDoom123");
-	//connection 
-	if(!$db){
-  		echo "Connection failed";
-  	}
-	//code to find greatest uid and increment
-	$query = "SELECT uid FROM Users ORDER BY uid DESC LIMIT 1";
-	$result = pg_query($db, $query);
-	$arr = pg_fetch_row($result);
-	$arr[0] = $arr[0] +1; 
-
-	if(isPrivate == "true" || "TRUE"){
-		$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]',TRUE,'$_POST[hashedpw]')";
-		$result2 = pg_query($db,$query2);
-	}
-	else{
-		$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]',FALSE,'$_POST[hashedpw]')";
-		$result2 = pg_query($db,$query2);
-	}
-	
-?>  
 
 <?php 
 	print( "Hello World<br />"); 
