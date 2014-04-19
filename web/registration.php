488bd7f830fab547de1879e8c480a6bc0ee6259a
ddachille <ddachille@ufl.edu>
2014-04-19 15:24:51 -0400

    Registration Working
    Definitely should work now.

--- web/registration.php
+++ web/registration.php
@@ -17,14 +17,8 @@
 	$arr = pg_fetch_row($result);
 	$arr[0] = $arr[0] +1; 
 
-	if(isPrivate == "true" || "TRUE"){
-		$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]',TRUE,'$_POST[hashedpw]')";
-		$result2 = pg_query($db,$query2);
-	}
-	else{
-		$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]',FALSE,'$_POST[hashedpw]')";
-		$result2 = pg_query($db,$query2);
-	}
+	$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]','$_POST[hashedpw]')";
+	$result2 = pg_query($db,$query2);
 	
 ?>  
 
