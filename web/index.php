0de714fbbab1fc0e65801ded447adfb5ad77eb69
ddachille <ddachille@ufl.edu>
2014-04-19 14:13:09 -0400

    Index.php
    Form to register user- working except it first enters an empty user
    before entering the user! (need to figure out)

--- web/index.php
+++ web/index.php
@@ -7,15 +7,34 @@
  
 <form name="index" action="index.php" method="POST" >  
 Username:<input type="text" name="username" />  
-Password:<input type="text" name="hashedpw" />     
+Password:<input type="text" name="hashedpw" />
+Set Profile to Private? (true or false): <input type="text" name= "isPrivate" />     
 <input type="submit" />  
 </form>  
 
 
-<?php  
-$db = pg_connect("host=postgres dbname=meal user= js7 password=MealAdminOfDoom123");  
-$query = "INSERT INTO Users VALUES(20, '$_POST[username]',TRUE ,'$_POST[hashedpw]')";
-$result = pg_query($db,$query);
+<?php 
+
+	$db = pg_connect("host=postgres dbname=meal user= js7 password=MealAdminOfDoom123");
+	//connection 
+	if(!$db){
+  		echo "Connection failed";
+  	}
+	//code to find greatest uid and increment
+	$query = "SELECT uid FROM Users ORDER BY uid DESC LIMIT 1";
+	$result = pg_query($db, $query);
+	$arr = pg_fetch_row($result);
+	$arr[0] = $arr[0] +1; 
+
+	if(isPrivate == "true" || "TRUE"){
+		$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]',TRUE,'$_POST[hashedpw]')";
+		$result2 = pg_query($db,$query2);
+	}
+	else{
+		$query2 = "INSERT INTO Users VALUES($arr[0], '$_POST[username]',FALSE,'$_POST[hashedpw]')";
+		$result2 = pg_query($db,$query2);
+	}
+	
 ?>  
 
 <?php 
