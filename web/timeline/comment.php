5bb667291f49bc578f5d466ed3d8177fa3446591
JoeyS7 <joeysiracusa@gmail.com>
2014-04-21 02:29:31 -0400

    Comments are GOOD

--- web/timeline/comment.php
+++ web/timeline/comment.php
@@ -20,21 +20,24 @@
   		echo "Connection failed";
   	}
 	
-	$query4 = "SELECT uid FROM Users WHERE Users.username =".$session_username;
+	$query4 = "SELECT uid FROM Users WHERE Users.username ='".$session_username."'";
 	$result4 = pg_query($db,$query4);
-	$arr3 = pg_fetch_row($result4);
+	$arr3 = pg_fetch_row($result4, 0);
 	$uid = $arr3[0];
+	echo "uid = ".$uid."<br>";
 	
 	//$query5 = "SELECT pid FROM (Users JOIN MakePost ON (Users.uid = MakePost.uid)) WHERE uid=".$uid;
 	//$result5 = pg_query($db,$query5);
 	//$arr2 = pg_fetch_row($result5);
 	//$pid = $arr2[0];
+	echo "pid = ".$_SESSION['pid']."<br>";
 	
 	//code to find greatest uid and increment
 	$query = "SELECT cid FROM Comment ORDER BY cid DESC LIMIT 1";
 	$result = pg_query($db, $query);
 	$arr = pg_fetch_row($result);
 	$arr[0] = $arr[0] +1; 
+	echo "cid = ".$arr[0]."<br>";
 
 	$query2 = "INSERT INTO Comment VALUES(".$arr[0].", '".$_POST['comment']."')";
 	$query3 = "INSERT INTO MakeComment VALUES(".$uid.", ".$arr[0].", ".$_SESSION['pid'].")";
@@ -49,7 +52,7 @@
 	//$_SESSION['login'] = "t";
 	//$_SESSION['username'] = $username;
 	
-   	header( "Location: postPage.php?pid=".$_GET['pid']) ;
+   	//header( "Location: postPage.php?pid=".$_SESSION['pid']) ;
 
 ?>  
 
