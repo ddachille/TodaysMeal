a594c9d422e3e5c28bb40ed2bbd449f37d71f185
JoeyS7 <joeysiracusa@gmail.com>
2014-04-21 02:03:01 -0400

    Fixing a session error

--- web/timeline/makeIngredient.php
+++ web/timeline/makeIngredient.php
@@ -18,10 +18,8 @@
 
 	echo '<pre>';
 	if (move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile)) {
-    	echo "File is valid, and was successfully uploaded.\n";
+		$_SESSION['imgpath'] = $uploadfile;
 	} else {
-    	echo "Possible file upload attack!\n";
-    	echo 'Here is some more debugging info:';
 		print_r($_FILES);
 	}
 	
